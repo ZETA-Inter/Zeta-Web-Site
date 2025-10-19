d4e4a1b91d1321d23e27c0a79e52b63ff3e46d68
Alisson1471 <alissonalvesdossantosoliveira@gmail.com>
2025-10-19 19:18:32 -0300

    feat: add list workerIds endpoint and list Goals by CompanyId

---
+++ src/services/goalService.js
@@ -0,0 +1,58 @@
+const token = "TokenUserZeta1234"
+const postgres_url = "http://localhost:8080"
+
+async function listGoalsByCompanyId(companyId) {
+  try {
+    const res = await fetch(`${postgres_url}/api/goals/list-goals-by-company?companyId=${companyId}`, {
+      method: "GET",
+      headers: {
+        "Authorization": `Bearer ${token}`,
+        "Content-Type": "application/json"
+      }
+
+    });
+  
+    if (!res.ok) {
+      throw new Error("Erro na requisição: " + res.status);
+    }
+      
+    const data = await res.json();
+    
+    console.log(`Lista de metas do fornecedor ${companyId}: `, data)
+    
+    return data;
+  } catch (err) {
+    console.error("Erro ao buscar produtores: ", err);
+    throw err;
+  }
+}
+
+async function listWorkerIdsByGoalId(goalId) {
+  try {
+    const res = await fetch(`${postgres_url}/api/worker-goals/list-worker-ids-by-goalId/${goalId}`, {
+        method: "GET",
+        headers: {
+          "Authorization": `Bearer ${token}`,
+          "Content-Type": "application/json"
+        }
+    });
+
+    if (!res.ok) {
+        throw new Error("Erro na requisição: " + res.status);
+    }
+    
+    const data = await res.json();
+  
+    console.log(`Lista de worker Ids da meta ${goalId}: `, data)
+  
+    return data;
+    } catch (err) {
+        console.error("Erro ao buscar produtores: ", err);
+        throw err;
+    }
+}
+
+export default {
+  listGoalsByCompanyId,
+  listWorkerIdsByGoalId,
+};
